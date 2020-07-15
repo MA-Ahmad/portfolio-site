fd689caf3b89a40c9a5363ca3cb84be9202afaf7
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-07-15 10:13:32 -0700

    Added gallery page content

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container, Row, Col, Image } from "react-bootstrap";
 
 const Gallery = () => {
-  return <div></div>;
+  return (
+    <Container className="gallery-container">
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src="/assets/images/slider/image3.jpg" thumbnail />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="/assets/images/slider/image3.jpg" thumbnail />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="/assets/images/slider/image3.jpg" thumbnail />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Gallery;
