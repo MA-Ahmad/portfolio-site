d8ddf6bf2b71d179e5caf7b6e45576c696808d2c
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-07-15 09:56:57 -0700

    Added react-router-dom NavLink for active link

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { Navbar, Nav, FormControl, Button, Form } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#company">Company</Navbar.Brand>
         <Nav className="mr-auto">
-          <Nav.Link href="/home" className="active">
+          <NavLink to="/home" className="nav-link">
             <span className="fa fa-home fa-lg"></span>Home
+          </NavLink>
+          <NavLink to="/gallery" className="nav-link">
+            <span className="fa fa-picture-o fa-lg"></span>Gallery
+          </NavLink>
+          <Nav.Link href="#features" className="nav-link">
+            Features
           </Nav.Link>
-          <Nav.Link href="/gallery">Gallery</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#pricing" className="nav-link">
+            Pricing
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="#aboutus">
+            <span className="fa fa-info fa-lg"></span> About Us
+          </Nav.Link>
+          <Nav.Link className="nav-link" href="#contactus">
+            <span className="fa fa-address-card fa-lg"></span> Contact Us
+          </Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
