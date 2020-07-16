9d132134949241e45e18c029f34130f138c37f0e
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2020-07-16 10:39:16 -0700

    gallery page css updated

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -4,7 +4,7 @@ import { Container, Row, Col, Image } from "react-bootstrap";
 const Gallery = () => {
   return (
     <Container className="gallery-container">
-      <Row>
+      <Row className="gallery-row">
         <Col xs={6} md={4}>
           <Image src="/assets/images/slider/image3.jpg" thumbnail />
         </Col>
@@ -15,7 +15,7 @@ const Gallery = () => {
           <Image src="/assets/images/slider/image3.jpg" thumbnail />
         </Col>
       </Row>
-      <Row className="row2">
+      <Row className="gallery-row">
         <Col xs={6} md={4}>
           <Image src="/assets/images/slider/image3.jpg" thumbnail />
         </Col>
